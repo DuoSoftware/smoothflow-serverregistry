73a4191fd60f88e0f3d4775c3539960305917ae5
KalanaDemel2 <kalana@duosoftware.com>
2017-08-08 10:44:34 +0530

    update

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ server.post('/webhook/publish', function (req, res, next) {
 
     console.log('wget  http://dev.smoothflow.io/engine/'+execloc +' -P /home/sflow/PublishedDockers/'+req.body.FolderName);
 
-    var output = sh.exec('wget  http://dev.smoothflow.io/engine/'+execloc +' -P /home/sflow/PublishedDockers/'+req.body.FolderName ,{silent:false}).stdout;
+    var output = sh.exec('wget  http://dev.smoothflow.io/engine/'+execloc +' -P /home/sflow/PublishedDockers/'+req.body.FolderName+'/'+req.body.Tenant ,{silent:false}).stdout;
     console.log(output);
     output = sh.exec('/home/sflow/smooth.sh '+ req.body.DockerName +' '+ req.body.Tag +' '+ req.body.FolderName +' '+'/home/sflow/PublishedDockers/'+ req.body.FolderName +' '+ req.body.PortA +' '+ req.body.PortB +' '+ req.body.ProcessName +' '+ req.body.RAM +' '+ req.body.CPU +' '+ req.body.SecurityToken +' '+ req.body.Tenant ,{silent:false}).stdout;
     console.log(output);
